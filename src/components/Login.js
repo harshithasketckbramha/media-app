a6f9bd07afa59506a0191f21eafac2cfc0302dda
shailja22 <shailja@sketchbrahma.com>
2022-06-09 16:13:44 +0530

    background

--- src/components/Login.js
+++ src/components/Login.js
@@ -32,7 +32,7 @@ const fetchToken=async()=>{
   try{
     const response=await axios.get("https://api.themoviedb.org/3/authentication/token/new?api_key=84bf934f6f348e09a8de2b9b556c09ae")
     console.log(response.data);
-    setToken(response.data.request_token)
+    // setToken(response.data.request_token)
     const Token_Key=response.data.request_token
     localStorage.setItem("users",Token_Key)
     
